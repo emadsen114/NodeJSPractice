430f9d641779c3298b7f4981f8b03c6aa6dba3a2
Emily <emily.madsen03@gmail.com>
2024-07-11 14:56:49 -0700

    surprise and no surprise html files, additions to index.js

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
 const express = require('express');
 const { readFile } = require('fs');
+const path = require('path');
 
 const app = express();
 
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-
-
-app.get('/',(request, response) =>{
-    readFile('home.html', 'utf8', (err, html)=> {
-        if(err){
-            response.status(500).send('Sorry its broken, but i love you anyways!');
+// Route for the home page
+app.get('/', (request, response) => {
+    readFile(path.join(__dirname, 'home.html'), 'utf8', (err, html) => {
+        if (err) {
+            console.error('Error reading home.html:', err);
+            response.status(500).send('Sorry, something went wrong!');
+            return;
         }
-
-        response.send(html)
+        response.send(html);
     });
-})
-app.listen(process.env.PORT|| 3000, () => console.log('App available on http://localhost:3000'));
-// app.get ('/', (the request, the response) => {
-//
-//});
+});
+
+// Route for the surprise page
+app.get('/surprise', (request, response) => {
+    readFile(path.join(__dirname, 'surprise.html'), 'utf8', (err, html) => {
+        if (err) {
+            console.error('Error reading surprise.html:', err);
+            response.status(500).send('Sorry, something went wrong!');
+            return;
+        }
+        response.send(html);
+    });
+});
+
+app.get('/noSurprise', (request, response) => {
+    readFile(path.join(__dirname, 'noSurprise.html'), 'utf8', (err, html) => {
+        if (err) {
+            console.error('Error reading noSurprise.html:', err);
+            response.status(500).send('Sorry, something went wrong!');
+            return;
+        }
+        response.send(html);
+    });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('App available on http://localhost:3000');
+});
